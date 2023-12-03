b4b6f50890e7cf5b060af45cc4d70a7b8e5e80dc
Sudo-Self <sudo-self@proton.me>
2023-12-03 06:17:21 -0700

    Update LiveCursors.tsx

--- src/components/LiveCursors.tsx
+++ src/components/LiveCursors.tsx
@@ -36,7 +36,7 @@ const LiveCursors = () => {
       ref={liveCursors}
       className={`example-container ${styles.liveCursorsContainer}`}
     >
-      <p style={{ maxWidth: "80%", textAlign: "center" }}>Welcome to the Same Page!</p>
+      <p style={{ maxWidth: "80%", textAlign: "center" }}>🧀 🍷 Welcome to the same page!</p>
       <YourCursor self={self as Member | null} parentRef={liveCursors} />
       <MemberCursors />
     </div>
